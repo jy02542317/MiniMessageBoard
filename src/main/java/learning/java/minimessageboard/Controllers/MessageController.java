0c1b92442ae32dcd5e5d18156d17e57d71d1c61a
Chengjie Xu <chengjie.xu@pwc.com>
2023-07-14 10:46:51 +0800

    update file table

--- src/main/java/learning/java/minimessageboard/Controllers/MessageController.java
+++ src/main/java/learning/java/minimessageboard/Controllers/MessageController.java
@@ -6,4 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/Message")
 public class MessageController {
+    /*分为两部分
+    * 1.创建Message，带文件 2.展示MessageList*/
 }
