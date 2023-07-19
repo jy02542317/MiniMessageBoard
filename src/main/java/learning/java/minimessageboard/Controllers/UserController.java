06b95fab599f872890f08438c10710e110a5d3c5
Chengjie Xu <chengjie.xu@pwc.com>
2023-07-19 15:31:09 +0800

    author init

--- src/main/java/learning/java/minimessageboard/Controllers/UserController.java
+++ src/main/java/learning/java/minimessageboard/Controllers/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -61,14 +62,16 @@ public class UserController {
     public void deleteUser(@RequestParam Long id) {
         userServices.DeleteUser(id);
     }
+
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/deleteUsers")
     public void deleteUsers(@RequestParam Long[] ids) {
         userServices.DeleteUsers(Arrays.stream(ids).toList());
     }
+
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PostMapping("/getUserById")
-    public TbUserEntity getUserById(@RequestParam Long id) {
-        return userServices.getTbUserById(id).get();
+    public TbUserEntity getUserById(@RequestParam Long id)  {
+        return userServices.getTbUserById(id).orElse(new TbUserEntity());
     }
 }
